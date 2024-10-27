abc36d2f785ad269ecaa956aff08c5194b572f2c
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-27 21:37:44 +0100

    Add C array and C build tasks

--- src/array/main.c
+++ src/array/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  printf("array");
+
+  int arr[] = {0, 1, 2, 3, 4, 5, 6, 7};
+  for(int i = 0; i < 8; ++i) {
+    printf("%p\n", &arr[i]);
+  }
+
   return 0;
 }
