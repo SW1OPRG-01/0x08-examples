d05fbf8f7c6d2e965e2bd938951f82b833e45565
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-28 22:06:33 +0100

    Clean up

--- src/function-pointers/vector2d.c
+++ src/function-pointers/vector2d.c
@@ -1,16 +1,15 @@
 #include "vector2d.h"
 #include <stdio.h>
-// Function to initialize a Vector2D struct
+
 Vector2D init(int x, int y, calculate calc) {
     Vector2D p;
     p.x = x;
     p.y = y;
-    p.display = print;  // Default display function
-    p.calculate = calc; // Set calculate function pointer
+    p.display = print;  
+    p.calculate = calc; 
     return p;
 }
 
-// Display function
 void print(Vector2D *p) {
     printf("Vector2D(%d, %d)\n", p->x, p->y);
 }
