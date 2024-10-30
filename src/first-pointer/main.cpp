bff82019c60c33e5529b320d812fb219b1bf5251
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-30 05:15:27 +0100

    Fix address printing

--- src/first-pointer/main.cpp
+++ src/first-pointer/main.cpp
@@ -1,9 +1,13 @@
 #include "integer.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
   
   Integer i1;
   i1.setInteger(1);
+  cout << hex << &i1 << endl;
 
   return 0;
 }
