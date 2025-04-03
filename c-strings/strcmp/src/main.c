1c2e0a917b1c4cbdda9f89e52441d2b6d71c8a6f
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2025-04-03 20:58:54 +0200

    Add string projects

--- c-strings/strcmp/src/main.c
+++ c-strings/strcmp/src/main.c
@@ -6,10 +6,10 @@ int main() {
   char *code_msys = "SW1MSYS-01";
 
   int result = strcmp(code_oprg, code_msys);
-  printf("%d\n ", result);
+  printf("%d\n", result);
   
   result = strcmp(code_msys, code_oprg);
-  printf("%d\n ", result);
+  printf("%d\n", result);
 
   return 0;
 }
